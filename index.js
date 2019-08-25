b3c39749d3fb75769da99508c422237bcf24fd13
田建辉 <tianjianhui@tianjianhuideMacBook-Pro.local>
2019-08-25 22:46:24 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+function transeformColor(val,num) {
+    if(val.length>4){
+        return 'rgba(' + parseInt(val.slice(1,3),16) + ',' + parseInt(val.slice(3,5),16) + ',' + parseInt(val.slice(5),16) + ',' + num||'' + ')'
+    }else{
+        return 'rgba(' + parseInt(val.slice(1,2)+val.slice(1,2),16) + ',' + parseInt(val.slice(2,3)+val.slice(2,3),16) + ',' + parseInt(val.slice(3)+val.slice(3),16) + ',' + num||'' + ')'
+    }
+}
+export default transeformColor
